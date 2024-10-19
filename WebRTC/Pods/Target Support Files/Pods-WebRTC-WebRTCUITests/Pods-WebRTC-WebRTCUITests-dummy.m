6eeef96fd315fd6769327ea0f22e9fa9a9e1ac06
Baran Karaoğuz <barankaraoguz@Mac.hgw.local>
2024-10-19 13:34:05 +0300

    feat: Change project name

--- WebRTC/Pods/Target Support Files/Pods-WebRTC-WebRTCUITests/Pods-WebRTC-WebRTCUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WebRTC_WebRTCUITests : NSObject
-@end
-@implementation PodsDummy_Pods_WebRTC_WebRTCUITests
-@end
--- WebRTC/Pods/Target Support Files/Pods-WebRTCTests/Pods-WebRTCTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WebRTCTests : NSObject
-@end
-@implementation PodsDummy_Pods_WebRTCTests
-@end
